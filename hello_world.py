ed7c38e5fd5efb22df1a4982793bffc55920e65a
aleksimyllyniemi <129107820+aleksimyllyniemi@users.noreply.github.com>
2025-01-24 10:36:24 +0200

    mqtt

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,18 @@
-print("hello world")
+
+import paho.mqtt.client as mqtt			# Tuodaan kirjastot​
+
+def on_connect(client, userdata, flags, rc):		# Yhteyden muodostuessa​
+
+    if rc == 0:					# suoritetaan tämä funktio​
+        print("Connect OK")​
+
+    else:
+        print(f"Connect fail. Error: {rc}")​
+
+client = mqtt.Client() 				# Tehdään client​
+
+client.on_connect = on_connect 			# Liiteään on_connect clientin funktioon​
+
+client.connect("mosquitton.ip.tähän.näin", 1883, 60) 	# Muodostetaan yhteys​
+
+client.loop_forever()
