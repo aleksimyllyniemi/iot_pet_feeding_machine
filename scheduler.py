25ed0251391bfe4e2a66301bf3cf576f3f4a3766
EmpKon <emppu.kontola@gmail.com>
2025-02-12 00:45:31 +0200

    Remote scheduler (WIP)

--- scheduler.py
+++ scheduler.py
@@ -10,7 +10,7 @@ print(currTime)
 
 
 def setTimes(h1=None,h2=None):
-    a=re.search('^(?:[01]\d|2[0-3]):[0-5]\d$', h1)
+    a=re.search('^(?:[01]\d|2[0-3]):[0-5]\d$', h1) #Regex, tarkistaa että syöte on väliltä 00:00-23:59
     b=re.search('^(?:[01]\d|2[0-3]):[0-5]\d$', h2)
     if a != None and b != None:
         pass
