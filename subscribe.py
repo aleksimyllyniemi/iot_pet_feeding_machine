b1f12f7ad92181d3309a0667c42ef59665e01e89
EmpKon <emppu.kontola@gmail.com>
2025-02-12 16:13:28 +0200

    Code cleanup

--- subscribe.py
+++ subscribe.py
@@ -1,24 +1,20 @@
 import paho.mqtt.client as mqtt			# Tuodaan kirjastot​
 #from stepperi import fullRev
 import multiprocessing
-import scheduler
 import time
 import datetime
 import re
 import os
+import signal
 
 
-t1="15:10"
-t2="22:30"
-
-
-badinput="Enter time in (HH:MM) format!"
 
 def setTimes(h1=None,h2=None):
+    badinput="Enter time in (HH:MM) format!"
     print(h1)
     print(h2)
-    a=re.search('^(?:[01]\d|2[0-3]):[0-5]\d$', h1) #Regex, tarkistaa että syöte on väliltä 00:00-23:59
-    b=re.search('^(?:[01]\d|2[0-3]):[0-5]\d$', h2)
+    a=re.search(r'^(?:[01]\d|2[0-3]):[0-5]\d$', h1) #Regex, tarkistaa että syöte on väliltä 00:00-23:59
+    b=re.search(r'^(?:[01]\d|2[0-3]):[0-5]\d$', h2)
     if a != None and b != None:
         return [a,b]
     else:
@@ -31,7 +27,7 @@ def on_connect(client, userdata, flags, rc):		# Yhteyden muodostuessa suoritetaa
     client.subscribe("tite24")		#   Tilataan clientille rasp/topic aihe​
 
 
-def counter(t1, t2): #multiprocessing testausta
+def counter(t1, t2):
     print("reset")
     early=min(t1,t2)
     late=max(t1,t2)
@@ -54,27 +50,16 @@ def on_message(client, userdata, msg):		# Viestin saapuessa suoritetaan tämä f
     print(message)
     msgtype=message[:3]
     if msgtype == "cfg":
-        #found_event.set()
-        #etä konfigurointi
         times=setTimes(message[4:9],message[10:15])
         print(times)
-        #p2.kill()
-        time.sleep(1)
         print("cfg viesti")
-        #p2.start()
-        #if type(times)==Str:
-
-        pass
     elif msgtype == "spn":
         #fullRev()
         pass
         
-        
-
-
-
 
 def mqttc():
+
     client = mqtt.Client()				# Luodaan client
     
     client.on_connect = on_connect			# Clientin on_connect metodiksi liitetään yo. on_connec​
@@ -90,9 +75,11 @@ def mqttc():
 
 
 if __name__=="__main__":
+    t1="15:10"
+    t2="22:30"
     found_event=multiprocessing.Event()
-    p2 = multiprocessing.Process(target=counter, args=(t1,t2)) #multiprocessing testausta
+    p2 = multiprocessing.Process(target=counter, args=(t1,t2))
     p1 = multiprocessing.Process(target=mqttc, args=())
-    while True:
-        p2.start()
-        p1.start()
+    
+    p2.start()
+    p1.start()
