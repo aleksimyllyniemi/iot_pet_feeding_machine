f5cd50f58605b2a101b9ac5f4854bb0e48c3af41
EmpKon <145741072+EmpKon@users.noreply.github.com>
2025-05-14 15:05:48 +0300

    Renamed and implemented HTTP requests

--- petfeeder.py
+++ petfeeder.py
@@ -2,7 +2,11 @@ import paho.mqtt.client as mqtt			# Tuodaan kirjastot​
 from stepperi import fullRev
 import time
 import datetime
+import random
+import requests
+import json
 from grove.display.jhd1802 import JHD1802
+
 #from gpiozero import Buzzer
 
 lcd = JHD1802()
@@ -24,23 +28,34 @@ def setTime():
     time.sleep(1)
     buzzer.off()"""
 
-
-
+def sendmsg(outp):
+	client.publish("tite24", payload=outp, qos=0, retain=False)
 
 def on_message(client, userdata, msg):		# Viestin saapuessa suoritetaan tämä funktio​
     message=msg.payload.decode()
+    print(message)
     msgtype=message[:3]
+    now = str(datetime.datetime.now())[:19]
+    #timea = str(datetime.datetime.now())
+    amt = random.randint(100,130) #mock data
     if msgtype == "spn":
         fullRev()
         setTime()
-        #beep()
-        
+        sendmsg(f"Ruokittu {now} {amt}g")
+        lähetys = {"mittaus" : (now, amt)}
+        viesti = json.dumps(lähetys)
+        requests.post("https://tite24-mittaukset-ek-ahdefafzdtcsaqhh.northeurope-01.azurewebsites.net/lisaa_tieto", data=viesti)
     elif msgtype == "spd":
         for i in range(3):
             fullRev()
-        setTime()
-        #beep()
-        
+        sendmsg(f"Ruokittu {now} {amt*3}g")
+        lähetys = {"mittaus" : (now, amt*3)}
+        viesti = json.dumps(lähetys)
+        requests.post("https://tite24-mittaukset-ek-ahdefafzdtcsaqhh.northeurope-01.azurewebsites.net/lisaa_tieto", data=viesti)
+    elif message=="Väärä syöte" or message[0:8]=="Ruokittu":
+        pass
+    else:
+	    sendmsg("Väärä syöte")
 
 
 client = mqtt.Client()				# Luodaan client
@@ -51,7 +66,7 @@ client.on_message = on_message			# Clientin on_message metodiksi liitetään yo.
 
 client.will_set('tite24', b'{"status": "Off"}')	# Kerrotaan brokerille LWT aihe (topic) ja viesti
 
-client.connect("test.mosquitto.org", 1883, 60)	# Yhdistetään client MQTT serveriin (brokeriin)​
+client.connect("broker.emqx.io", 1883, 60)	# Yhdistetään client MQTT serveriin (brokeriin)​
 
 client.loop_forever()				# Toistetaan
 
@@ -59,3 +74,4 @@ client.loop_forever()				# Toistetaan
 
 if __name__=="__main__":
     pass
+
